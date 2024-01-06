4fa2a0c284379a9e70f396d9e4c461d275cab842
kgy1008 <gayeon1008@g.skku.edu>
2024-01-07 01:37:10 +0900

    server

--- main.js
+++ main.js
@@ -21,7 +21,6 @@ const pool = mysql.createPool({
 const app = express();
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-app.use('/public',static(path.join(__dirname,'public')));
 
 app.post('/register', (req, res) =>{
     console.log('post 인식');
@@ -63,19 +62,36 @@ app.post('/login', (req, res)=>{
   pool.query('select id, password from user where id=? and password=?', [id,pw], (err, data)=>{
     if(data.length == 0){ // 로그인 실패
       console.log('로그인 실패');
-      res.status(200).send("");
+      res.status(200).json({token:"",id:""});
     }
     else{
       // 로그인 성공
       console.log('로그인 성공');
       pool.query('select id from user where id=?',[id],(err,data)=>{
-        res.status(200).send(data[0]); 
+        const token = jwt.sign({ id }, 'your_secret_key', { expiresIn: '1h' }); // 토큰 생성
+        res.status(200).json({ token, id });
       });
       
     }
   });
 
 });
+
+function verifyToken(req, res, next) {
+  const token = req.headers['authorization'];
+
+  if (!token) {
+    return res.status(403).json({ message: '토큰이 제공되지 않았습니다.' });
+  }
+
+  jwt.verify(token, 'your_secret_key', (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ message: '유효하지 않은 토큰입니다.' });
+    }
+    req.userId = decoded.id;
+    next();
+  });
+}
 
 app.post('/login/idcert', (req, res) =>{
   console.log('/login/idcert의 post 인식');
@@ -112,11 +128,35 @@ app.get('/boardclass', (req, res) => {
   });
 });
 
-// 게시글 목록
-app.post('/getBoardPosts', (req, res) => {
-  const selectedBoard = req.body.boardName; // 클라이언트에서 선택한 게시판 이름
+app.post('/boardclass/create', (req, res) => {
+  const newBoardName = req.body.newtitle;
+  const creator = req.body.creater;
 
-  pool.query('SELECT * FROM bulletin WHERE board = ?', [selectedBoard], (err, data) => {
+  // 이미 존재하는 게시판인지를 확인하는 SELECT 쿼리를 사용하지 않고, 바로 삽입 쿼리를 실행합니다.
+  pool.query('INSERT INTO board (name, creater) VALUES (?, ?)', [newBoardName, creator], (err, data) => {
+    if (err) {
+      // 오류 발생 시, 이미 존재하는 게시판인지 확인합니다.
+      console.error(err);
+      if (err.code === 'ER_DUP_ENTRY') {
+        console.log('이미 존재하는 게시판입니다.');
+        res.status(200).json({"success": false});
+      } else {
+        console.log('게시판 생성 실패');
+        res.status(500).json({"success":false});
+      }
+    } else {
+      console.log('게시판 생성 성공');
+      res.status(200).json({"success": true});
+    }
+  });
+});
+
+
+// 게시글 목록
+app.post('/board', (req, res) => {
+  const selectedBoard = req.body.name; // 클라이언트에서 선택한 게시판 이름
+
+  pool.query('SELECT author,title,context,board FROM posts WHERE board = ?', [selectedBoard], (err, data) => {
       if (err) {
           res.status(500).send(err);
       } else {
